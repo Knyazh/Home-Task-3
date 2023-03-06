1b7b274ea94622f0435b7ce87625646f81c9cf27
Hp <qnyazface@maul.ru>
2023-03-06 04:33:19 -0800

    Check

--- Home Task 3/Home Task 3/Program.cs
+++ Home Task 3/Home Task 3/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string firstBrandName = "Adidas";
+int firstBrandcount = 4;
+int firstBrandprice = 200;
+int firsttotalPrice = firstBrandcount * firstBrandprice;
+
+
+Console.WriteLine("Firma :" +  firstBrandName);
+Console.WriteLine("Ayaqqabi sayi :" +  firstBrandcount);
+Console.WriteLine("Birinin qiymeti ($) :" + firstBrandprice);
+Console.WriteLine("Hamısının birlikdə qiymeti ($) :" + firsttotalPrice);
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+
+
+string secondBrandName = "Gucci";
+int seconBrandcount = 2;
+int secondBrandprice = 1000;
+int secondtotalPrice = seconBrandcount * secondBrandprice;
+
+Console.WriteLine("Firma :" + secondBrandName);
+Console.WriteLine("Ayaqqabi sayi :" + seconBrandcount);
+Console.WriteLine("Birinin qiymeti ($) :" + secondBrandprice);
+Console.WriteLine("Hamısının birlikdə qiymeti ($) :" + secondtotalPrice);
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+
+string thirdBrandName = "Nike";
+int thirdBrandcount = 1;
+int thirdBrandprice = 150;
+int thirdtotalPrice = thirdBrandcount * thirdBrandprice;
+
+Console.WriteLine("Firma :" + thirdBrandName);
+Console.WriteLine("Ayaqqabi sayi :" + thirdBrandcount);
+Console.WriteLine("Birinin qiymeti ($) :" + thirdBrandprice);
+Console.WriteLine("Hamısının birlikdə qiymeti ($) :" + thirdtotalPrice);
+
